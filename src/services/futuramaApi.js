3004c8a5d0b4c6e513daae7f8247a9bdf94a06a8
EmilyDSarani <emily.sarani@gmail.com>
2022-01-10 18:45:58 -0600

    got the fetch figured out

--- src/services/futuramaApi.js
+++ src/services/futuramaApi.js
@@ -1,1 +1,15 @@
 //will do the logic to grab the futurama api here
+
+//fetch all characters
+export const getCharacters = async () => {
+    const res = await fetch( 'futuramaapi.herokuapp.com/api/v2/characters')
+    const characters = await res.json();
+    return characters;
+}
+
+//fetch character quotes
+export const getCharacterQuotes = async () => {
+    const res = await fetch('futuramaapi.herokuapp.com/api/characters/<character name>')
+    const quotes = await res.json();
+    return quotes
+}
