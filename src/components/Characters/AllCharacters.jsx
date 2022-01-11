d887a9d2ea4a9f46c5c1445d1ed2bc6960723cca
EmilyDSarani <emily.sarani@gmail.com>
2022-01-10 23:23:11 -0600

    will logic this out later

--- src/components/Characters/AllCharacters.jsx
+++ src/components/Characters/AllCharacters.jsx
@@ -2,10 +2,20 @@
 //probably will need a link to the character detail page
 import React from 'react'
 
-export default function AllCharacters() {
+export default function AllCharacters({characters}) {
     return (
-        <div>
-            
-        </div>
+        <ul>
+            {characters.map((character) =>(
+                <li>
+                    <Link to={`/characters/${character.name}`}>
+                        <div>
+                            <img src={character.image}
+                                alt={character.name} />
+                            <h1>{character.name}</h1>
+                        </div>
+                    </Link>
+                </li>
+            ))}
+        </ul>
     )
 }
--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,12 +1,27 @@
 //will get a list of characters
 //will pass it down to a component that will render how the list will look
 //use effect will probs set the characters
-import React from 'react'
+import { useEffect, useState} from 'react';
+import AllCharacters from '../../components/Characters/AllCharacters';
+import { getCharacters } from '../../services/futuramaApi';
 
 export default function List() {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    //this use effect is saying- get all the characters and set them to the res. once they are loaded, set loading back to false.
+    useEffect(() => {
+        const loadCharacters = async() => {
+            const res = await getCharacters();
+            setCharacters(res);
+            setLoading(false);
+        };
+        loadCharacters();
+    }, []);
+
     return (
         <div>
-            
+            {loading && <AllCharacters characters={characters}/>}
         </div>
     )
 }
